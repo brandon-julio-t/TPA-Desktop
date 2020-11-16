daba6c7eafa77458a5b1c913808094733e65e573
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-11-16 21:24:08 +0700

    finalization

--- TPA Desktop/Core/Strategies/Views/SecurityAndMaintenanceViewStrategy.cs
+++ TPA Desktop/Core/Strategies/Views/SecurityAndMaintenanceViewStrategy.cs
@@ -1,10 +1,10 @@
-﻿using TPA_Desktop.Core.Interfaces;
-using TPA_Desktop.Views.Departments.Security_and_Maintenance;
+﻿using System;
+using TPA_Desktop.Core.Interfaces;
 
 namespace TPA_Desktop.Core.Strategies.Views
 {
     public class SecurityAndMaintenanceViewStrategy : IStrategy
     {
-        public void Execute() => new SecurityAndMaintenanceWindow().Show();
+        public void Execute() => throw new NotImplementedException("Security and Maintenance is not yet developed.");
     }
 }
